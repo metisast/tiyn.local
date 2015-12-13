2439e86558476be59b4c16f90454ad4154d40b92
metisast <metis_ast@mail.ru>
2015-12-13 21:09:21 +0600

    Test PC

--- resources/lang/en/interface.php
+++ resources/lang/en/interface.php
@@ -24,6 +24,7 @@
         'userAdmin' => 'Administrator',
 
         /*Guest*/
+        'guestWelcome' => 'Welcome!'
 
 
     ];
--- resources/views/guest/template/_start.blade.php
+++ resources/views/guest/template/_start.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
+    <title>{{ trans('interface.guestWelcome') }}</title>
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta content="utf-8" http-equiv="encoding">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
